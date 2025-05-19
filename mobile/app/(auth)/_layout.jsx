3a0ce2559250ff831928d103741cf316e618cddb
Slip2188 <81004201+Slip2188@users.noreply.github.com>
2025-05-19 18:00:35 +0530

    Added setup for frontend

---
+++ mobile/app/(auth)/_layout.jsx
@@ -0,0 +1,11 @@
+// 'rnf' is shortcut for the boilerplate _layout.jsx file
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
---
+++ mobile/assets/styles/signup.styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from "react-native";
+import COLORS from '../../constants/colors';
+
+const styles = StyleSheet.create({
+    container: {
+        width: 20,
+
+    },
+    menu: {
+        font: "",
+    }
+})
