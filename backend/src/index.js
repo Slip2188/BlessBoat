685832754c7c71d9a73c3e2576960b7f6df88c56
Slip2188 <81004201+Slip2188@users.noreply.github.com>
2025-07-17 19:28:24 +0530

    Complete basic backend api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config"
 
 import authRoutes from "./routes/authRoutes.js";
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/auth", authRoutes); // basically if we want to visit any authentication routes (login or register), we have to add /api/auth in the address
 app.use("/api/journal", journalRoutes);
--- backend/src/routes/journalRoutes.js
+++ backend/src/routes/journalRoutes.js
@@ -5,6 +5,7 @@ import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// Creates new journal
 router.post("/", protectRoute, async (req, res) => {
     try {
         const journalName = req.body;
@@ -26,6 +27,39 @@ router.post("/", protectRoute, async (req, res) => {
     }
 });
 
+
+// Gets all the journals
+router.get('/', protectRoute, async (req, res)=> {
+    try {
+        const allJournals = await Journal.find({author: req.user._id}).sort({ updatedAt: -1 })
+        // const totalJournals = await Journal.countDocuments({author: req.user._id}) 
+        res.json(allJournals)
+    } catch (error) {
+        console.log("Error in fetching entries: ", error)
+        res.status(500).json({message:"Internal Server Error"})
+    }
+})
+
+// Deletes an journal
+router.delete(":/id", protectRoute, async (req, res)=>{
+    try {
+        const journal = await Journal.findById(req.params.id) // :/id and req.params.id is corelated i.e. if its :/hello then req.params.hello should be written
+        if (!journal) return res.status (404).json({ message: "Book not found" });
+
+        // Check if the journal is actually belonging to the user
+        if (journal.user.toString() !== req.user._id.toString()) return res.status (404).json({ message: "Server error" });
+
+        await journal.deleteOne()
+
+        res.json({message:"Journal Deleted Successfully"})
+    } catch (error) {
+        
+    }
+})
+
+/*
+
+// Creates new entry in current journal 
 router.post("/entry", protectRoute, async (req, res) => {
     // All this is assuming that the request will have the current journal in it for us to access 
     try {
@@ -65,9 +99,20 @@ router.post("/entry", protectRoute, async (req, res) => {
     }
 });
 
+
+// Gets all the entries of a journal
 router.get('/', protectRoute, async (req, res)=> {
+
+    // example call from react native - frontend
+    // const response = await fetch ("http://localhost:3000/api/journal?page=1&limit=5");
+
     try {
         const journalName = req.body;
+
+        // This is for pagination i.e. loading of the entries teen teen at a time, not evth at a time
+        const page = req.query.page || 1;
+        const limit = 3;
+        const skip = (page-1)*limit; // Skips loading of first (n-1)*limit entries where n is the page number
         
         if (!journalName) {
             return res.status(400).json({message: "Select journal to write entry in!"});
@@ -76,14 +121,28 @@ router.get('/', protectRoute, async (req, res)=> {
         const currentJournal = await Journal.find({name: journalName, author: req.user._id})
 
         const entries = await Journal.find({_id: currentJournal._id}, 'entries')
+            .sort({createdAt:-1}) // Gives the entries in descending order
+            .skip(skip)
+            .limit(limit);
+            //.populate("user", "username") -> used to access info about user who has created this journal. Not applicable here 
 
-        res.send(entries)
+        const totalEntries = await Journal.find({_id: currentJournal._id}, 'entries').count() // yaha error aa saktae
+
+        res.send({
+            entries,
+            currentPage: page,
+            totalEntries,
+            totalPages: Math.ceil(totalEntries/limit)
+        })
 
     } catch (error) {
         console.log("Error in fetching entries: ", error)
         res.status(500).json({message:"Internal Server Error"})
     }
 })
+
+
+*/
 
 
 export default router;
